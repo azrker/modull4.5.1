64b40219622821920cea0e073fe24fbb84a208d0
Azer <114072753+azrker@users.noreply.github.com>
2022-11-27 07:46:57 -0800

    Add files via upload

---
+++ my-app2/src/App.js
@@ -0,0 +1,13 @@
+import React,{Component} from 'react'
+import Dice from './Dice'
+
+export default class App extends Component{
+  render(){
+    return(
+      <div>
+        <Dice />
+      </div>
+    )
+  }
+}
+
